82d3fcf4adcc28c1a1cb099598fea2a1081d7428
Fabio Colletto <149891035+fabiocolletto@users.noreply.github.com>
2025-12-23 17:03:47 -0300

    Update manifest.js

--- cells/manifest.js
+++ cells/manifest.js
@@ -1,7 +1,4 @@
-// Manifesto declarativo de células do Genoma v1.2
-// Cada entrada deve seguir o formato: { name: 'celula.nome', module: './celulas/arquivo.js' }
-
 export const cellsManifest = [
-  { name: 'sis_genoma', module: './core/genoma.js' },
-  { name: 'app_school', module: './cells/school/index.js' }
+  { name: 'sis_genoma', module: './core/genoma.js', icon: "Home"  },
+  { name: 'app_school', module: './cells/school/index.js', icon: "School" }
 ];
